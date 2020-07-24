9d57a2ca9537b994986a9199cc2dea7a8fc3f79e
Hasnain <mhali241997@gmail.com>
2020-07-25 01:26:46 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, StatusBar, Text } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+
+class App extends Component {
+  componentDidMount() {
+    // SplashScreen.hide();
+  }
+  
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: 'pink', alignItems: 'center', justifyContent: 'center' }}>
+        <StatusBar backgroundColor='white' barStyle='dark-content' />
+
+        <Text>Hello</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
